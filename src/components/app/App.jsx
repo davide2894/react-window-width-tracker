380f84287f68f3a400806a43e01e2158ac6f260d
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-06-14 00:16:54 +0200

    caused a memory leak that will fix later with useEffect cleanup

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,30 @@
 import './App.scss';
+import { useState } from 'react';
 import WindowWidthTracker from '../windowWidthTracker/WindowWidthTracker';
 
+/**
+ * Challenge:
+ * 1. Create state called `show`, default to `true`
+ * 2. When the button is clicked, toggle `show`
+ * 3. Only display `<WindowTracker>` if `show` is `true`
+ */
+
 function App() {
+  const [show, setShow] = useState(false);
+  
+  function handleClick(){
+    setShow(prevState => !prevState);
+  }
+
   return (
     <div className="app">
-      <button className="app__button">
+      <button 
+        className="app__button"
+        onClick={handleClick}  
+      >
         Toggle WindowTracker
       </button>
-      <WindowWidthTracker/>
+      { show && <WindowWidthTracker/>}
     </div>
   );
 }
--- src/components/windowWidthTracker/WindowWidthTracker.jsx
+++ src/components/windowWidthTracker/WindowWidthTracker.jsx
@@ -1,9 +1,19 @@
 import "./WindowWidthTracker.scss";
+import {useEffect, useState} from "react";
 
 function WindowWidthTracker() {
+    const [windowWidth, setWindowWidt] = useState(window.innerWidth);
+    console.log({windowWidth});
+
+    useEffect(() => {
+       window.addEventListener("resize", function() {
+           setWindowWidt(window.innerWidth);
+       }); 
+    }, [])
+
     return (
         <h1 className="tracker">
-            Window width: {window.innerWidth}
+            Window width: {windowWidth}
         </h1>
     )
 }
