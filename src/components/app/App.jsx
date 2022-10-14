f4fcd1aed22cedcb5c0f8fcbe19333f58785c3c7
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-10-14 17:21:36 +0200

    docs: remove comments

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,13 +2,6 @@ import './App.scss';
 import { useState } from 'react';
 import WindowWidthTracker from '../windowWidthTracker/WindowWidthTracker';
 
-/**
- * Challenge:
- * 1. Create state called `show`, default to `true`
- * 2. When the button is clicked, toggle `show`
- * 3. Only display `<WindowTracker>` if `show` is `true`
- */
-
 function App() {
   const [show, setShow] = useState(false);
   
@@ -20,7 +13,7 @@ function App() {
     <div className="app">
       <button 
         className="app__button"
-        onClick={handleClick}  
+        onClick={handleClick}
       >
         Toggle WindowTracker
       </button>
